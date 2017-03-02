bb3ebcd347926ad1f1116663eed6721e11a0e70a
bartekjagiello <bartekjagiello@hotmail.com>
2017-03-02 16:27:57 +0100

    'dasdas'

--- flaskapp.py
+++ flaskapp.py
@@ -25,8 +25,8 @@ def sendmsg():
     to = request.args.get('to')
     msg = request.args.get('msg')
     token = request.args.get('token')
-	loginExt = request.args.get('login')
-	passwordExt = request.args.get('password')
+    loginExt = request.args.get('login')
+    passwordExt = request.args.get('password')
     if(str(token) == expected_token):
         client = Client(login=loginExt, password=passwordExt)
         res = client.send_message(to, msg)  
